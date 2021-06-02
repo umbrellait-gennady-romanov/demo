e506e579f7ab7b1609f66234fabbde9f3987cf13
gennadyromanov <gennady.romanov@umbrellait.com>
2021-06-02 14:25:03 +0300

    2 commit

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -2,9 +2,14 @@ package com.demo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {SpringApplication.run(DemoApplication.class, args);	}
+	public static void main(String[] args) {
+		ApplicationContext app = SpringApplication.run(DemoApplication.class, args);
+		Client client = app.getBean(Client.class);
+		client.printName();
+	}
 }
